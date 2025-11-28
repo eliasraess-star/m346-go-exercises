77e7ba5ebcf5fda75dab4fe7c69ed6e8a39b73ec
Elias Raess <elias_raess@sluz.ch>
2025-11-28 12:11:49 +0100

    solving go-2-ex-2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -6,10 +6,15 @@ func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
 	fibs[2] = fibs[0] + fibs[1]
+	fibs[3] = fibs[2] + fibs[1]
+	fibs[4] = fibs[3] + fibs[2]
 	// TODO: correct up to index 4 using direct element access
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
+	// TODO: replace 0 with the next Fibonacci number
+	fibs = append(fibs, fibs[4]+fibs[3]) // 8
+	fibs = append(fibs, fibs[5]+fibs[4]) // 13
+	fibs = append(fibs, fibs[6]+fibs[5]) // 21
 	// TODO: compute three more Fibonacci numbers and append them
-
+	fibs = append(fibs, fibs[6]+fibs[7])
 	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
 }
