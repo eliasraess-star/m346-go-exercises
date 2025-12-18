d12843f1a40abcab41e08240619623b10f4142e9
Elias Raess <elias_raess@sluz.ch>
2025-12-18 19:29:11 +0100

    solving go-4-ex-1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,16 @@
 package main
 
+import "fmt"
+
 // TODO: implement the function computeGrade
+func computeGrade(E float32, M float32) float32 {
+	// function body
+	var N float32 = (E/M)*5 + 1
+	return N
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	fmt.Println(computeGrade(17.5, 28.0)) // 4.125
+	fmt.Println(computeGrade(28.0, 28.0)) // 6.0
+	fmt.Println(computeGrade(0.0, 28.0))  // 1.0
 }
