536396fa210713944a36d0e55e086ee1ac7100d7
Elias Raess <elias_raess@sluz.ch>
2025-12-18 19:36:07 +0100

    solving go-4-ex-2

--- go-4-ex-2/main.go
+++ go-4-ex-2/main.go
@@ -1,7 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 // TODO: implement the function computeHypotenuse using math.Pow and math.Sqrt
+func computeHypotenuse(a float64, b float64) float64 {
+	// function body
+	var hypotenuse = math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
+	return float64(hypotenuse)
+}
 
 func main() {
 	// TODO: call the function computeHypotenuse
+
+	fmt.Print(computeHypotenuse(3, 4)) // 5
 }
